7136dcb35de88ec65a56eacf5c885fd675fe6cd4
neels <neels.sunira@gmail.com>
2015-08-09 07:28:13 -0700

    Drawer activity

--- app/src/main/java/com/l904/MainActivity.java
+++ app/src/main/java/com/l904/MainActivity.java
@@ -1,26 +1,56 @@
 package com.l904;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.l904.database.DbController;
 
 public class MainActivity extends AppCompatActivity {
 
     private DbController dbController;
+    private DrawerLayout Drawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbController = DbController.getInstace();
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();
+       /* dbController = DbController.getInstace();
         dbController.openDb(this);
         dbController.insertExpense("Neels Exp", "100Rs");
         dbController.insertTodo("Shopping", "Market");
         dbController.insertTodo("Shopping","Market");
         dbController.insertItem("sugar","milk",1);
-        Utils.copyDBToExternal(this);
+        Utils.copyDBToExternal(this);*/
 
     }
 
